9498d69d48fc3e6460f87207249f1e4a4f201386
Patel Rudra <patelrudra7337@gmail.com>
2023-03-07 23:02:36 +0530

    all error solved

--- routes/UserRoutes.js
+++ routes/UserRoutes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController= require('../controllers/UserController')
 
-// router.get('/user',userController.getUserDataWithRole)
+
 router.post('/user',userController.addUser)
 router.get('/user/:id',userController.getUserById)
 router.delete('/user/:id',userController.deleteUser)
